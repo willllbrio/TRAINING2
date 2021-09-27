eed37ef0b4ee917c3b66a9b43d57efb7c34e0f1e
WILLIAM <williamlkengne@gmail.com>
2021-09-27 14:56:50 +0000

    [ADD] INHERITED MODULE

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy Modele to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+     
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['sale'],
+    
+    'data': [
+        'security/academy_security.xml',
+        'security/ir.model.access.csv', 
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',  
+        'views/session_views.xml',
+        'views/sale_views_inherit.xml',
+       
+    ],
+    
+    'demo': [
+       'demo/academy_demo.xml',
+        
+    ],
+    
+}
